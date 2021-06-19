7f593083b4266eacc99f7edd90a9c6b378dbd695
Rajesh Kumar <rajesh.k@avanzasolutions.com>
2021-06-20 02:00:01 +0500

    Initial Commit

---
+++ Status/Status.h
@@ -0,0 +1,18 @@
+//
+//  Status.h
+//  Status
+//
+//  Created by Rajesh Kumar Sahil on 20/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Status.
+FOUNDATION_EXPORT double StatusVersionNumber;
+
+//! Project version string for Status.
+FOUNDATION_EXPORT const unsigned char StatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Status/PublicHeader.h>
+
+
